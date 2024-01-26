ac3c5681fffebc5abd87308ebc0429e3eaa5b4cf
heghine <heghine.balasanyan@gmail.com>
2024-01-26 13:14:41 +0400

    upd: fix start

--- src/cub_map_read.c
+++ src/cub_map_read.c
@@ -6,7 +6,7 @@
 /*   By: hbalasan <hbalasan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 01:05:59 by hbalasan          #+#    #+#             */
-/*   Updated: 2023/11/28 19:36:45 by hbalasan         ###   ########.fr       */
+/*   Updated: 2024/01/26 13:13:43 by hbalasan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,11 @@ void	check_textures(char	*line, t_cub *c, int n[0][2])
 		free(line);
 		cub_error(no_mem, 1);
 	}
+	if (!*dir ||!*(dir + 1))
+	{
+		free(line);
+		cub_error(inv_tex, 1);
+	}
 	if (!ft_strncmp(dir[0], "NO", 3))
 		c->tex.n_bak = get_tex(mlx_load_img(c->mlx_ptr, dir[1]), &c->tex.n, n);
 	else if (!ft_strncmp(dir[0], "SO", 3))
@@ -30,9 +35,10 @@ void	check_textures(char	*line, t_cub *c, int n[0][2])
 		c->tex.e_bak = get_tex(mlx_load_img(c->mlx_ptr, dir[1]), &c->tex.e, n);
 	else if (!ft_strncmp(dir[0], "WE", 3))
 		c->tex.w_bak = get_tex(mlx_load_img(c->mlx_ptr, dir[1]), &c->tex.w, n);
-	if ((!ft_strncmp(dir[0], "F", 2) && c->tex.floor == -1) || \
-		(!ft_strncmp(dir[0], "C", 2) && c->tex.ceiling == -1))
+	if (((!ft_strncmp(dir[0], "F", 2) && c->tex.floor == -1) || \
+		(!ft_strncmp(dir[0], "C", 2) && c->tex.ceiling == -1)))
 			get_cf_colors(c, dir);
+	// add the check if it's not valid
 	ft_free_matrix(&dir);
 }
 
@@ -53,7 +59,10 @@ void	read_map(char *file, t_cub *cub)
 		line[1] = ft_strtrim(line[0], "\n");
 		free(line[0]);
 		if (line[1] && line[1][0] && ++i[0] < 6)
+		{
+			// check_cf
 			check_textures(line[1], cub, &i);
+		}
 		else if ((line[1] && line[1][0]) || i[0] >= 6)
 			cub->map = ft_extend_matrix(cub->map, line[1]);
 		if ((int)ft_strlen(line[1]) > cub->width)
